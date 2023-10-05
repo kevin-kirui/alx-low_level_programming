1bc55532baca095e1feb6cfdba8dbe4e9d32586e
kevin kirui <kipkoechkevin42@gmail.com>
2023-10-05 16:08:13 -0700

    -a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <unistd.h>
 #include <stdio.h>
-#include <string.h>
 /**
  * main - Entry point
  *
  * Return: Always 1 (Success)
  */
 int main() {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return 1;
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
