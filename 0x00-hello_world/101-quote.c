e39fd4feb8a11bba988486f14f34fc1ca617c025
kevin kirui <kipkoechkevin42@gmail.com>
2023-10-05 16:03:02 -0700

    -a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main() {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
