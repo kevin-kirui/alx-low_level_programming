d626dbc655950885ad923196bd45130ae09e60af
kevin kirui <kipkoechkevin42@gmail.com>
2023-10-09 13:15:03 -0700

    -a

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,21 @@
-     1#include <stdio.h>
-     3/**
-     4* main - Entry point
-     5*
-     6* Return: Always 0 (Success)
-     7*/
-     9 int main (void)
-    10{
-    11	char c;
-    13		for (c = 'a'; c <= 'z'; c++)
-    15			putchar (c);
-    16			putchar ('\n');
-    17	return (0);
-    18	}
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints a text according number
+ *
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+		putchar('\n');
+	}
+	return (0);
+}
