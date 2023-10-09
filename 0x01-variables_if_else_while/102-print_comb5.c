2d5482b899fd50847342be91f59edfa869e7b408
kevin kirui <kipkoechkevin42@gmail.com>
2023-10-09 09:52:57 -0700

    -a

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,40 +7,24 @@
   */
 int main(void)
 {
-	int c, i, k, j;
+	int a, b;
 
-	for (c = 48; c <= 57; c++)
+	for (a = 0; a < 99; a++)
 	{
-		for (i = 48; i <= 57; i++)
+	for (b = (a + 1); b < 100; b++)
+	{
+		putchar('0' + (a / 10));
+		putchar('0' + (a % 10));
+		putchar(' ');
+		putchar('0' + (b / 10));
+		putchar('0' + (b % 10));
+		if (!(a == 98 && b == 99))
 		{
-			for (k = 48; k <= 57; k++)
-			{
-				for (j = 48; j <= 57; j++)
-				{
-					if (((k + j) > (c + i) &&  k >= c) || c < k)
-					{
-						putchar(c);
-						putchar(i);
-						putchar(' ');
-						putchar(k);
-						putchar(j);
-
-					if (c + i + k + j == 227 && c == 57)
-					{
-					break;
-					}
-					else
-					{
-					putchar(',');
-					putchar(' ');
-					}
-					}
-				}
-			}
+		putchar(',');
+		putchar(' ');
 		}
 	}
-
-	putchar('\n');
-
-	return (0);
-}
+	}
+	putchar("\n");
+	return(0);
+}	
