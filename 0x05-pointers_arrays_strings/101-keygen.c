d140dff22ff58641c0d345a1c9d54e6af60d2f13
kevin kirui <kipkoechkevin42@gmail.com>
2023-10-17 10:08:36 -0700

    -a

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,28 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "main.h"
 
-#define PASSWORD_LENGTH 16
 
-/**
- * _atoi - Convert a string to an integer.
- * @s: The pointer to convert
- *
- * Return: A integer
- */
-int main(void)
-{
-    srand(time(0));
+#define PASSWORD_LENGTH 10
 
-    char password[PASSWORD_LENGTH + 1];
+int main(void) {
+    srand(time(0)); 
+
+    char password[PASSWORD_LENGTH + 1]; 
     const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     int i;
 
     for (i = 0; i < PASSWORD_LENGTH; i++) {
         password[i] = charset[rand() % (sizeof(charset) - 1)];
     }
-    password[PASSWORD_LENGTH] = '\0';
+    password[PASSWORD_LENGTH] = '\0'; 
 
     printf("Generated Password: %s\n", password);
 
