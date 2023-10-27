7216a22662a2c8124b67474309aeaab2e1a1febb
kevin kirui <kipkoechkevin42@gmail.com>
2023-10-27 09:42:12 -0700

    -a

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,27 +11,24 @@
   */
 int main(int argc, char *argv[]) 
 {
+	
 	if (argc != 2) 
 	{
-		printf("Error\n");
 		return 1;
 	}
-	int cents = atoi(argv[1]);
-	if (cents < 0) 
+int cents = atoi(argv[1]);
+	if (cents < 0)
 	{
-		printf("0\n");
 		return 0;
 	}
-	int numCoins = 0;
-	numCoins += cents / 25;
-	cents %= 25;
-	numCoins += cents / 10;
-	cents %= 10;
-	numCoins += cents / 5;
-	cents %= 5;
-	numCoins += cents / 2;
-	cents %= 2;
-	numCoins += cents;
-	printf("%d\n", numCoins);
-	return 0;
+	int coins[] = {25, 10, 5, 2, 1};
+	int num_coins = sizeof(coins) / sizeof(coins[0]);
+    	int count = 0;
+	
+		for (int i = 0; i < num_coins; i++) 
+		{
+        	count += cents / coins[i];
+        	cents %= coins[i];
+		}
+		return 0;
 }
