0d11a16253b73ae16a36effc9e8e580b581fbb2f
kevin kirui <kipkoechkevin42@gmail.com>
2023-10-30 14:12:30 -0700

    -a

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,15 +3,14 @@
 
 /**
   * free_grid -gird memory freed
-  * @grid: grid 
-  * @height: height 
+  * @grid:grid 
+  * @height:height 
   *
   * Return: 1
   */
 void free_grid(int **grid, int height)
 {
 	int i;
-	
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
