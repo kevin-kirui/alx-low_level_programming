bfcc1935e4921108acc1462d8c81772faf741d3b
kevin kirui <kipkoechkevin42@gmail.com>
2023-10-30 07:28:32 -0700

    -a

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,35 +3,50 @@
 #include <stdlib.h>
 
 /**
-  * main - Prints the sum of args positive numbers
+  * main - Prints minimum coins to make change
   * @argc: argument count
   * @argv: argument vector
   *
   * Return: Always zero
   */
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-	if (argc != 2) 
-	{
-        printf("Error\n");
-        return 1;
-	}
-	int cents = atoi(argv[1]);
-		
-		if (cents < 0) 
-		{
-        	printf("0\n");
-        	return 0;
-		}
-	int coins[] = {25, 10, 5, 2, 1};
-    	int num_coins = sizeof(coins) / sizeof(coins[0]);
-    	int count = 0;
-	
-		for (int i = 0; i < num_coins; i++) 
-		{
-        	count += cents / coins[i];
-        	cents %= coins[i];
-		}
-	printf("%d\n", count);
-	return 0;
+        int cents, coins = 0;
+
+        if (argc != 2)
+        {
+                printf("Error\n");
+                return (1);
+        }
+
+        cents = atoi(argv[1]);
+
+        while (cents > 0)
+        {
+                coins++;
+                if ((cents - 25) >= 0)
+                {
+                        cents -= 25;
+                        continue;
+                }
+                if ((cents - 10) >= 0)
+                {
+                        cents -= 10;
+                        continue;
+                }
+                if ((cents - 5) >= 0)
+                {
+                        cents -= 5;
+                        continue; }
+                if ((cents - 2) >= 0)
+                {
+                        cents -= 2;
+                        continue;
+                }
+                cents--;
+        }
+
+        printf("%d\n", coins);
+
+        return (0);
 }
