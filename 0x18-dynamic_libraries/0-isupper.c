889f2e09dfc034179f9c2371bc56417dd9d8b6af
kevin kirui <kipkoechkevin42@gmail.com>
2024-01-15 12:41:12 -0800

    Dynamic libraries

--- 0x18-dynamic_libraries/0-isupper.c
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-  * _isupper - Check if a letter is upper
-  * @z: The number to be checked
-  *
-  * Return: 1 for upper letter or 0 for any else
-  */
-int _isupper(int x)
+ * _isupper - Check if a letter is upper
+ * @z: The number to be checked
+ *
+ * Return: 1 for upper letter or 0 for any else
+ */
+int _isupper(int c)
 {
-	if (x >= 65 && x <= 90)
+	if (c >= 65 && x <= 90)
 	{
 		return (1);
 	}
