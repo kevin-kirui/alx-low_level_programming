1b7171b0f5468305bda43114b275bcd047ea6f63
kevin kirui <kipkoechkevin42@gmail.com>
2024-01-15 12:13:50 -0800

    Dynamic  libraries

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - Returns string length
+ * @s: String to be measured
+ *
+ * Return: String length
+ */
+int _strlen(char *s)
+{
+    int len = 0;
+
+    while (*s != '\0') {
+        len++;
+        s++;
+    }
+
+    return len;
+}
